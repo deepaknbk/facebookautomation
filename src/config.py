ab312afc4442a4a774c12e258c9e97baef572c49
Deepak Nallajerla <deepaknbk@gmail.com>
2021-03-06 22:28:20 -0600

    post to multiple FB groups
    - Created a loop to post images to multiple groups
    - Post with multiple images

--- src/config.py
+++ src/config.py
@@ -1,16 +1,19 @@
 
-https://www.facebook.com/groups/2512957342342147 - Dev Test
-https://www.facebook.com/groups/473152883710976 - Test2
-https://www.facebook.com/groups/751322535510799 - Test4
-https://www.facebook.com/padati.jewels - Padati Jewels
+# https://www.facebook.com/groups/2512957342342147 - Dev Test
+# https://www.facebook.com/groups/473152883710976 - Test2
+# https://www.facebook.com/groups/751322535510799 - Test4
+# https://www.facebook.com/padati.jewels - Padati Jewelsd
+
+image_path="C:/Users/User/PycharmProjects/facebookautomation/images/"
 
 FB_group_props= {
-    'DevTest': {
+
+'DevTest': {
         'url':"https://www.facebook.com/groups/2512957342342147",
         'wall_post_xpath':"/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div[2]/div/div/div[4]/div/div/div/div/div[1]/div[1]/div/div/div/div[1]/div/div[1]/span",
         'popup_post_xpath':"/html/body/div[1]/div/div[1]/div/div[4]/div/div/div[1]/div/div[2]/div/div/div/div/div[1]/form/div/div[1]/div/div/div[1]/div[2]/div[1]/div[1]/div[1]/div/div/div/div/div[2]/div",
         'post_img_flag':True,
-        'upload_img_xpath':"/html/body/div[1]/div/div[1]/div/div[4]/div/div/div[1]/div/div[2]/div/div/div/div/div[1]/form/div/div[1]/div/div/div[1]/div[3]/div[1]/div[2]/div[1]/input",
+        'upload_img_input_xpath':"/html/body/div[1]/div/div[1]/div/div[4]/div/div/div[1]/div/div[2]/div/div/div/div/div[1]/form/div/div[1]/div/div/div[1]/div[3]/div[1]/div[2]/div[3]/input",
         'submit_post':"/html/body/div[1]/div/div[1]/div/div[4]/div/div/div[1]/div/div[2]/div/div/div/div/div[1]/form/div/div[1]/div/div/div[1]/div[3]/div[2]/div[1]/div"
     },
 'Test2': {
@@ -18,7 +21,19 @@ FB_group_props= {
         'wall_post_xpath':"/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div[4]/div/div/div/div/div[1]/div[1]/div/div/div/div[1]/div/div[1]/span",
         'popup_post_xpath':"/html/body/div[1]/div/div[1]/div/div[4]/div/div/div[1]/div/div[2]/div/div/div/div/div[1]/form/div/div[1]/div/div/div[1]/div[2]/div[1]/div[1]/div[1]/div/div/div/div/div[2]/div",
         'post_img_flag':True,
-        'upload_img_xpath':"/html/body/div[1]/div/div[1]/div/div[4]/div/div/div[1]/div/div[2]/div/div/div/div/div[1]/form/div/div[1]/div/div/div[1]/div[3]/div[1]/div[2]/div[2]/input",
+        'upload_img_input_xpath':"/html/body/div[1]/div/div[1]/div/div[4]/div/div/div[1]/div/div[2]/div/div/div/div/div[1]/form/div/div[1]/div/div/div[1]/div[3]/div[1]/div[2]/div[3]/input",
         'submit_post':"/html/body/div[1]/div/div[1]/div/div[4]/div/div/div[1]/div/div[2]/div/div/div/div/div[1]/form/div/div[1]/div/div/div[1]/div[3]/div[2]/div/div"
+
+    },
+
+'PadatiJewels': {
+        'url':"https://www.facebook.com/padati.jewels",
+        'wall_post_xpath':"/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div/div[4]/div[2]/div/div[2]/div[1]/div/div/div/div/div[1]/div/div[1]/span",
+        'popup_post_xpath':"/html/body/div[1]/div/div[1]/div/div[4]/div/div/div[1]/div/div[2]/div/div/div/form/div/div[1]/div/div/div[2]/div[1]/div/div[1]/div[1]/div/div/div/div/div[2]/div",
+        'post_img_flag':False,
+        'upload_img_input_xpath':"/html/body/div[1]/div/div[1]/div/div[4]/div/div/div[1]/div/div[2]/div/div/div/form/div/div[1]/div/div/div[3]/div[1]/div[2]/div/div[1]/input",
+        'submit_post':"/html/body/div[1]/div/div[1]/div/div[4]/div/div/div[1]/div/div[2]/div/div/div/form/div/div[1]/div/div/div[3]/div[2]/div"
     }
+
+
 }
--- src/fb.py
+++ src/fb.py
@@ -1,119 +1,65 @@
 from selenium import  webdriver
 import time
-import collections
-import getpass
+import config
+import os
 from selenium.webdriver.common.keys import Keys
 
 PATH="C:\Program Files (x86)\chromedriver.exe"
 
+def get_image_path():
+
+    img_files=os.listdir(config.image_path)
+    return img_files
 
 def facebook_group_post(user,pwd):
-    # Use a breakpoint in the code line below to debug your script.
-    url = "https://www.facebook.com/groups/2512957342342147"
-    url2="https://www.facebook.com/groups/473152883710976"
     chrome_options = webdriver.ChromeOptions()
     pref = {"profile.default_content_setting_values.notifications": 2}
     chrome_options.add_experimental_option("prefs", pref)
-    driver = webdriver.Chrome(PATH,chrome_options=chrome_options)
+    driver = webdriver.Chrome(PATH, chrome_options=chrome_options)
+
+    #Login to facebook
+    driver.get("https://www.facebook.com/")
+    time.sleep(5)
+    driver.find_element_by_name("email").send_keys(user)
+    driver.find_element_by_name("pass").send_keys(pwd, Keys.RETURN)
+    time.sleep(10)
 
     try:
-        driver.get(url)
-        time.sleep(5)
-        driver.find_element_by_name("email").send_keys(user)
-        driver.find_element_by_name("pass").send_keys(pwd,Keys.RETURN)
-        #driver.find_element_by_name("pass").send_keys(Keys.RETURN)
-        time.sleep(10)
-        #temp=driver.find_element_by_xpath("a8c37x1j")
 
-        post=driver.find_element_by_xpath("/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/"
-                                          "div[2]/div/div/div[4]/div/div/div/div/div[1]/div[1]/div/div/div/div[1]/div/div[1]/span")
+        for k,v in config.FB_group_props.items():
+            print(f"Posting contenet in {k} group/page")
+            driver.get(v['url'])
+            time.sleep(5)
 
-        time.sleep(10)
-        post.click()
-        time.sleep(5)
-        post_popup=driver.find_element_by_xpath("/html/body/div[1]/div/div[1]/div/div[4]/div/div/div[1]/div/"
-                                                "div[2]/div/div/div/div/div[1]/form/div/div[1]/div/div/div[1]/"
-                                                "div[2]/div[1]/div[1]/div[1]/div/div/div/div/div[2]/div")
-        time.sleep(10)
-        post_popup.send_keys("Hi Test post, take7")
+            # Find the element to click on the post text box on the wall
+            driver.find_element_by_xpath(v['wall_post_xpath']).click()
+            time.sleep(10)
 
-        # upload_img=driver.find_element_by_xpath("/html/body/div[1]/div/div[1]/div/div[4]/div/div/div[1]/"
-        #                                         "div/div[2]/div/div/div/div/div[1]/form/div/div[1]/div/"
-        #                                         "div/div[1]/div[3]/div[1]/div[2]/div[1]/input")
-        # #print(len(upload_img))
-        #
-        # time.sleep(10)
-        #
-        # upload_img.send_keys("C:/Users/User/PycharmProjects/facebookautomation/images/download.jpg")
+            #Write the text on the pop-up to post
+            driver.find_element_by_xpath(v['popup_post_xpath']).send_keys("Hi Test post, 3/6 Take8")
+            time.sleep(10)
 
-        post_msg=driver.find_element_by_xpath("/html/body/div[1]/div/div[1]/div/div[4]/div/div/div[1]/div/"
-                                              "div[2]/div/div/div/div/div[1]/form/div/div[1]/div/div/div[1]/"
-                                              "div[3]/div[2]/div[1]/div")
-        time.sleep(10)
-        post_msg.click()
-        print("Posted sucessfully in the group, open next URL")
-        time.sleep(5)
-        driver.get(url2)
-        time.sleep(5)
+            #Check if the Image Flag is set , if set upload the image
+            if v['post_img_flag']:
+                upload_img=driver.find_element_by_xpath(v['upload_img_input_xpath'])
+                time.sleep(5)
+                for file in os.listdir(config.image_path):
+                    upload_img.send_keys(config.image_path+file)
+                #upload_img.send_keys(get_image_path())
 
-        post = driver.find_element_by_xpath("/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div[4]/div/div/div/div/div[1]/div[1]/div/div/div/div[1]/div/div[1]/span")
-        time.sleep(10)
-        post.click()
-        time.sleep(5)
-        post_popup = driver.find_element_by_xpath("/html/body/div[1]/div/div[1]/div/div[4]/div/div/div[1]/div/"
-                                                  "div[2]/div/div/div/div/div[1]/form/div/div[1]/div/div/div[1]/"
-                                                  "div[2]/div[1]/div[1]/div[1]/div/div/div/div/div[2]/div")
-        time.sleep(10)
-        post_popup.send_keys("Hi Test post in new group, take3")
-        post_msg = driver.find_element_by_xpath("/html/body/div[1]/div/div[1]/div/div[4]/div/div/div[1]/div/div[2]/"
-                                                "div/div/div/div/div[1]/form/div/div[1]/div/div/div[1]/div[3]/div[2]/div/div")
-        time.sleep(10)
-        post_msg.click()
-        print("Posted sucessfully in the 2nd group")
+            #post the message and images
+            post_msg=driver.find_element_by_xpath(v['submit_post'])
+            time.sleep(5)
+            post_msg.click()
+            print(f"Posted sucessfully in the {k} group/page, opening next URL")
+            time.sleep(5)
 
-
-        #driver.find_element_by_class_name("a8c37x1j").send_keys("Hi this is test")
-
-        # for t in temp:
-        #
-        #     if 'Create a public post' in t.text:
-        #         print(t, t.text)
-        #         t.click()
-        #         time.sleep(10)
-        #         post_text=driver.find_elements_by_class_name("notranslate")
-        #
-        #
-        #
-        #         for p in post_text:
-        #             if p.get_attribute("role")=="textbox":
-        #                 p.send_keys("Hi test from Python")
-        #                 btn=driver.find_elements_by_class_name("oajrlxb2")
-        #
-        #
-        #                 for b in btn:
-        #                     if b.get_attribute("aria-label")=="Post":
-        #                         b.click()
-        #                 time.sleep(10)
-
-
-                # print(len(post))
-
-                #t.send_keys("Hi this is a test post")
-
-        # driver.find_element_by_class_name("thodolrn")
-        # groups = driver.find_elements_by_class_name("oajrlxb2")
-        # for group in groups:
-        #     if group.get_attribute("aria-label")=="Groups":
-        #         group.click()
-        #         time.sleep(10)
-        #         break
     except Exception as e:
         print("Exception Occurred: ",str(e))
     finally:
         print("Closing Browser....")
+        time.sleep(10)
         driver.close()
-
-    pass
 
 def get_login():
     user = input("Please enter FB User Name:")
